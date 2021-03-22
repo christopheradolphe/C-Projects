127bdaff61ed4f6c7afb987666c1d62d5bf0c0ab
Christopher Adolphe <52717277+christopheradolphe@users.noreply.github.com>
2021-03-21 22:03:16 -0400

    Add files via upload

---
+++ my_atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct node{
+    int data; 
+    struct node *next; 
+};
+
+
+int my_atoi(char *str){
+  int res = 0;
+  int size = 0;
+  while (str[size] != '\0'){
+      size++;
+  }
+  for(int i = 0; i<size; i++){
+    if(isdigit(str[i]) == 1){
+      res = res*10;
+      res = res + (str[i] - '0');
+    }
+  }
+  return res;
+}
+
+
+int main(void){
+  char s3[4] = "123";
+  int conversion = my_atoi(s3);
+  printf("Conversion: %d\n", conversion);
+
+  return 0;
+}
